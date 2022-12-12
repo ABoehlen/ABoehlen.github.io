01eec0b8aca7580f4be08333466331345ff3816a
ABoehlen <a.boehlen@bluewin.ch>
2022-12-12 16:25:46 +0100

    add script for generating the footer

--- js/news.js
+++ js/news.js
@@ -8,6 +8,7 @@
 var neuigkeiten = new Array();
 
 // Chronik der Aenderungen, neueste zuunterst
+//------------------------------------------------------------------------------------------------------------------------
 neuigkeiten[0] = "29.05.2022:<br />Übersicht <a href=\"./famous_five/famous_five.html\">Fünf Freunde</a> ergänzt";
 neuigkeiten[1] = "29.05.2022:<br />Neuer Bericht: <a href=\"./famous_five/ff_modell_eisenbahn_zeltlager.html\">Bahnanlagen «Zeltlager»</a>";
 neuigkeiten[2] = "19.06.2022: Startseite angepasst";
@@ -15,9 +16,12 @@ neuigkeiten[3] = "10.07.2022: Neue Funktion zum Vergrössern der Bilder";
 neuigkeiten[4] = "24.09.2022: Neue Rubrik <a href=\"./karten.html\">Karten</a> ergänzt";
 neuigkeiten[5] = "30.09.2022:<br />Übersicht <a href=\"./famous_five/famous_five.html\">Fünf Freunde</a> überarbeitet";
 neuigkeiten[6] = "22.10.2022: Karte <a href=\"./karten.html#2014_castlehill_map\">Castle Hill</a> ergänzt";
+neuigkeiten[7] = "12.12.2022: Neue Bilder Startseite / Footer in JavaScript ausgelagert";
+
+//------------------------------------------------------------------------------------------------------------------------
 
 // Definieren, ab welchem Element die Ausgabe erfolgen soll
-var min = 1;
+var min = 3;
 
 // Ausgabe der Aenderungen als Liste, neueste zuoberst
 document.write("<ul class=\"Liste\">");

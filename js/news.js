a917cb47838b59a123cb333ed29771dd38f33107
ABoehlen <a.boehlen@bluewin.ch>
2023-05-04 17:14:08 +0200

    modified

--- js/news.js
+++ js/news.js
@@ -10,19 +10,22 @@ var neuigkeiten = new Array();
 // Chronik der Aenderungen, neueste zuunterst
 //------------------------------------------------------------------------------------------------------------------------
 
-neuigkeiten[0] =  "29.05.2022: Übersicht <a href=\"./famous_five/famous_five.html\">Fünf Freunde</a> ergänzt";
-neuigkeiten[1] =  "29.05.2022: Neuer Bericht: <a href=\"./famous_five/ff_modell_eisenbahn_zeltlager.html\">Bahnanlagen «Zeltlager»</a>";
-neuigkeiten[2] =  "19.06.2022: Startseite angepasst";
-neuigkeiten[3] =  "10.07.2022: Neue Funktion zum Vergrössern der Bilder";
-neuigkeiten[4] =  "24.09.2022: Neue Rubrik <a href=\"./karten.html\">Karten</a> ergänzt";
-neuigkeiten[5] =  "30.09.2022: Übersicht <a href=\"./famous_five/famous_five.html\">Fünf Freunde</a> überarbeitet";
-neuigkeiten[6] =  "22.10.2022: Karte <a href=\"./karten.html#2014_castlehill_map\">Castle Hill</a> ergänzt";
-neuigkeiten[7] =  "12.12.2022: Neue Bilder Startseite / Footer in JavaScript ausgelagert";
-neuigkeiten[8] =  "29.01.2023: Karte <a href=\"./karten.html#2023_valledinibbio\">Valle di Nibbio</a> ergänzt";
-neuigkeiten[9] =  "01.03.2023: Dokumentation <a href=\"./secret_series/secret_maps_killi.html\"> Karten von Killimooin</a> erweitert";
-neuigkeiten[10] = "17.03.2023: Zu jeder Serienbeschreibung in <a href=\"./blyton.html\"> Enid Blyton</a> ein Bild ergänzt";
-neuigkeiten[11] = "22.03.2023: Neue Option auf der Startseite, um die gesamte Chronik anzuzeigen";
-neuigkeiten[12] = "30.04.2023: Dokumentation der Website und des Tools 'xref' im <a target=\"_blank\" href=\"https://github.com/ABoehlen/ABoehlen.github.io/blob/main/README.md\">README.md</a>.";
+neuigkeiten[0] =  "19.09.2021: Erstes Commit, noch ohne Inhalt";
+neuigkeiten[1] =  "27.01.2022: Inhalt ergänzt";
+neuigkeiten[2] =  "13.03.2022: Design optimiert";
+neuigkeiten[3] =  "29.05.2022: Übersicht <a href=\"./famous_five/famous_five.html\">Fünf Freunde</a> ergänzt";
+neuigkeiten[4] =  "29.05.2022: Neuer Bericht: <a href=\"./famous_five/ff_modell_eisenbahn_zeltlager.html\">Bahnanlagen «Zeltlager»</a>";
+neuigkeiten[5] =  "19.06.2022: Startseite angepasst";
+neuigkeiten[6] =  "10.07.2022: Neue Funktion zum Vergrössern der Bilder";
+neuigkeiten[7] =  "24.09.2022: Neue Rubrik <a href=\"./karten.html\">Karten</a> ergänzt";
+neuigkeiten[8] =  "30.09.2022: Übersicht <a href=\"./famous_five/famous_five.html\">Fünf Freunde</a> überarbeitet";
+neuigkeiten[9] =  "22.10.2022: Karte <a href=\"./karten.html#2014_castlehill_map\">Castle Hill</a> ergänzt";
+neuigkeiten[10] =  "12.12.2022: Neue Bilder Startseite / Footer in JavaScript ausgelagert";
+neuigkeiten[11] =  "29.01.2023: Karte <a href=\"./karten.html#2023_valledinibbio\">Valle di Nibbio</a> ergänzt";
+neuigkeiten[12] =  "01.03.2023: Dokumentation <a href=\"./secret_series/secret_maps_killi.html\"> Karten von Killimooin</a> erweitert";
+neuigkeiten[13] = "17.03.2023: Zu jeder Serienbeschreibung in <a href=\"./blyton.html\"> Enid Blyton</a> ein Bild ergänzt";
+neuigkeiten[14] = "22.03.2023: Neue Option auf der Startseite, um die gesamte Chronik anzuzeigen";
+neuigkeiten[15] = "30.04.2023: Dokumentation der Website und des Tools 'xref' im <a target=\"_blank\" href=\"https://github.com/ABoehlen/ABoehlen.github.io/blob/main/README.md\">README.md</a>.";
 
 //------------------------------------------------------------------------------------------------------------------------
 
